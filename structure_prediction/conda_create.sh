d78d013a669a640e17d7acc660f257c3446fe17d
Lim Heo <huhlim@gmail.com>
2023-03-16 13:36:17 -0400

    updated versions of dependent libraries

--- structure_prediction/conda_create.sh
+++ structure_prediction/conda_create.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-CUDA=11.0
+CUDA=11.5
 
-ENVNAME=af
+ENVNAME=af.test
 
-conda create --name $ENVNAME -y -c conda-forge -c nvidia \
-    python=3.8.5 \
+conda create --name $ENVNAME -y -c conda-forge -c nvidia -c bioconda\
+    python=3.8 \
     openmm=7.5.1 \
+    cudatoolkit=$CUDA \
+    cudnn=8.2 \
+    libcusolver  \
     mdtraj \
-    cudatoolkit=$CUDA.3 \
     pdbfixer \
-    pip \
-    absl-py=0.13.0  \
-    cudnn=8.0.4 \
-    libcusolver 
+    hhsuite \
+    kalign3 \
+    hmmer \
+    pip 
 
 source activate $ENVNAME
 
 pip install --upgrade pip 
 pip3 install -r requirements.txt 
-pip3 install --upgrade "jax[cuda${CUDA/./}]" jaxlib==0.1.69+cuda${CUDA/./} -f \
-      https://storage.googleapis.com/jax-releases/jax_releases.html
+pip3 uninstall -yq jax jaxlib
+pip3 install "jax[cuda]==0.3.25" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
-openmm_patch=$(readlink -f docker/openmm.patch)
+openmm_patch=$(readlink -f openmm.patch)
 cd $CONDA_PREFIX/lib/python3.8/site-packages
 patch -p0 < $openmm_patch
 cd -
@@ -32,3 +34,11 @@ if [[ ! -e alphafold/common/stereo_chemical_props.txt ]]; then
         https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt
 fi
 
+echo "####################################################"
+echo "# update binary_paths in libconfig_af.py as follows"
+echo "####################################################"
+for binary in jackhmmer hhblits hhsearch kalign hmmsearch hmmbuild;
+do
+    binary_path=$(which $binary)
+    echo "${binary}_binary_path = \"$binary_path\""
+done
